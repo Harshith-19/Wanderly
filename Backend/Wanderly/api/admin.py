c6cb13349f96c83fdbed1687a821468787d98187
Harshith-19 <harshith.jethu@gmail.com>
2023-11-04 18:50:05 +0530

    registered models

--- Backend/Wanderly/api/admin.py
+++ Backend/Wanderly/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Festival, Cities
+from .models import Festival, Cities, Places, Cuisine, Cart
 
 admin.site.register(Festival)
 admin.site.register(Cities)
+admin.site.register(Places)
+admin.site.register(Cuisine)
+admin.site.register(Cart)
